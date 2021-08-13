c587b6db1864c5175920f0cc912d9476ec5332fa
Zishu <zshnqmr1@gmail.com>
2021-08-13 14:32:15 +0530

    delete btn functionality added

--- index.js
+++ index.js
@@ -4,30 +4,37 @@ let myLeads = ["zeeshaquamar.com"]
 const inputEl = document.getElementById("input-el")
 const btn = document.getElementById("input-btn")
 const unEl = document.getElementById("un-el")
+const deleteBtn =document.getElementById("delete-btn")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderItem()
+    renderItem(myLeads)
 }
+
+function renderItem(items){
+    let listItem =""
+    for (let i = items.length-1; i>=0 ;i-- ){
+    listItem += `
+    <li>
+        <a target='_blank' href='${items[i]}'> ${items[i]}</a>
+    </li>`
+    }
+    unEl.innerHTML = listItem 
+}
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads = []
+    renderItem(myLeads)
+})
 
 btn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderItem();
+    renderItem(myLeads);
     console.log(localStorage.getItem("myLeads"))
 })
-function renderItem(){
-    let listItem =""
-    for (let i = myLeads.length-1; i>=0 ;i-- ){
-    listItem += `
-    <li>
-        <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]}</a>
-    </li>`
-    }
-    unEl.innerHTML = listItem 
-}

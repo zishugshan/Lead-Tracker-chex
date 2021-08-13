54d1cc3429d87abffa841d8560d467b747897d76
Zishu <zshnqmr1@gmail.com>
2021-08-13 11:59:24 +0530

    Localstorage leads are added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let myLeads = []
+let myLeads = ["zeeshaquamar.com"]
+
+
 const inputEl = document.getElementById("input-el")
 const btn = document.getElementById("input-btn")
 const unEl = document.getElementById("un-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+
+
 btn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderItem();
+    console.log(localStorage.getItem("myLeads"))
 })
 function renderItem(){
     let listItem =""
